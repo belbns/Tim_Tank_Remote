0216d76c0d5d6d91e54f900512c98112b7a769e6
belbns <belbns@gmail.com>
2019-12-18 17:31:14 +0300

    2019-12-18

--- app/src/main/java/com/example/timtankremote/MainActivity.java
+++ app/src/main/java/com/example/timtankremote/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.timtankremote;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -18,7 +19,9 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
@@ -32,16 +35,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int battAdcMin = 2200;
-    public static final int battMeterMax = 140;
-    public static final int battMeterDiv = 5;
-    public static final int battMeterLow = 40;
-    public static final int battMeterHigh = 80;
+    public static final double battMin = 10.8;
+    public static final double battMax = 12.6;
+    public static final double battLow = 11.1;
+    public static final double battHigh = 11.8;
+    public static final int battMeterMax = 180;
+    public static final int battK = 265;
+    public static final int p5p6K = 1241;
+
+    private static DecimalFormat df2 = new DecimalFormat(("#.##"));
 
     public static final int REQUEST_BLUETOOTH_ENABLE_CODE = 101;
     public static final int REQUEST_LOCATION_ENABLE_CODE = 101;
@@ -95,19 +103,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private class Queues {
+        public int common;
+        public int motors;
+        public int stepper;
+        public int servo;
+        public int dist;
+        public int leds;
+        public Queues() {
+            common = motors = stepper = servo = dist = leds = 0;
+        }
+    }
+
     private Motors motors;
     private Servo servo;
     private Stepper stepper;
+    private Queues queues;
+
     private int Leds[];
-    private int v3v3 = 0;
-    private int v6v = 0;
-    private int vP5 = 0;
-    private int vP6 = 0;
+    private int dist = 0;
+    private double v3v3 = 0;
+    private double v6v = 0;
+    private double vP5 = 0;
+    private double vP6 = 0;
+
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("   Управление танком");
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_battery_alert_black_24dp);
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, "Your devices that don't support BLE",
@@ -383,43 +413,147 @@ public class MainActivity extends AppCompatActivity {
                 if (jo.has("adc")) {
                     st = jo.getJSONArray("adc");
                     int numadc =  Integer.parseInt(st.getString(0));
-                    int valadc = Integer.parseInt(st.getString(1));
+                    double valadc = Double.parseDouble(st.getString(1));
                     switch (numadc) {
                         case 0:
-                            v3v3 = valadc;
-                            tvd.append("v3v3= " + v3v3);
-                            int meterV = (valadc - battAdcMin) / battMeterDiv;
+                            v3v3 = valadc / battK;
+                            double perc = v3v3 / battMax;
+                            int drawable;
+                            int color = Color.GREEN;
+                            if (perc > 0.9) {
+                                drawable = R.drawable.ic_battery_90_black_24dp;
+                            } else if (perc > 0.8) {
+                                drawable = R.drawable.ic_battery_80_black_24dp;
+                            } else if (perc > 0.6) {
+                                drawable = R.drawable.ic_battery_60_black_24dp;
+                                color = Color.YELLOW;
+                            } else if (perc > 0.5) {
+                                color = Color.YELLOW;
+                                drawable = R.drawable.ic_battery_50_black_24dp;
+                            } else if (perc > 0.3) {
+                                drawable = R.drawable.ic_battery_30_black_24dp;
+                                color = Color.RED;
+                            } else if (perc > 0.2) {
+                                drawable = R.drawable.ic_battery_20_black_24dp;
+                                color = Color.RED;
+                            } else {
+                                drawable = R.drawable.ic_battery_alert_black_24dp;
+                                color = Color.RED;
+                            }
+                            actionBar.setIcon(drawable);
+
+                            /*
+                            //tvd.append("v3v3= " + df2.format(v3v3));
+                            int meterV = (int) Math.round((v3v3 - battMin) * 100);
                             if (meterV < 0) {
                                 meterV = 0;
                             } else if (meterV > battMeterMax) {
                                 meterV = battMeterMax;
                             }
                             ProgressBar pb = findViewById(R.id.progressBatt);
-                            if (meterV > battMeterHigh) {
-
+                            int col = Color.GREEN;
+                            if (v3v3 < battLow) {
+                                col = Color.RED;
+                            } else if (v3v3 < battHigh) {
+                                col = Color.YELLOW;
                             }
+                            pb.setProgressTintList(ColorStateList.valueOf(col));
                             pb.setProgress(meterV);
+
+                             */
                             break;
                         case 1:
-                            v6v = valadc;
-                            tvd.append("v6v= " + v6v);
+                            v6v = valadc / battK;
+                            //tvd.append("v6v= " + df2.format(v6v));
                             break;
                         case 2:
-                            vP5 = valadc;
-                            tvd.append("vP5= " + vP5);
+                            vP5 = valadc / p5p6K;
+                            //tvd.append("vP5= " + df2.format(vP5));
                             break;
                         case 3:
-                            vP6 = valadc;
-                            tvd.append("vP6= " + vP6);
+                            vP6 = valadc / p5p6K;
+                            //tvd.append("vP6= " + df2.format(vP6));
                     }
                 } else if (jo.has("ms")) {
                     st = jo.getJSONArray("ms");
-
+                    String s = st.getString(0);
+                    if (s.equals("s") || s.equals("a")) {
+                        motors.state = s;
+                    } else if (s.equals("t") || s.equals("r") || s.equals("l")) {
+                        motors.vleft_real = Integer.parseInt(st.getString(1));
+                        motors.vright_real = Integer.parseInt(st.getString(2));
+                    } else if (s.equals("f") || s.equals("b")) {
+                        motors.vleft_real = motors.vright_real =
+                                Integer.parseInt(st.getString(1));
+                    }
+                } else if (jo.has("sv")) {
+                    st = jo.getJSONArray("sv");
+                    int num = Integer.parseInt(st.getString(0));
+                    if (num == 0) {
+                        stepper.angle_real = Integer.parseInt(st.getString(2));
+                    }
+                } else if (jo.has("servo")) {
+                    servo.angle_real = Integer.parseInt(jo.getString("servo"));
+                } else if (jo.has("dist")) {
+                    dist = Integer.parseInt(jo.getString("dist"));
+                } else if (jo.has("leds")) {
+                    st = jo.getJSONArray("leds");
+                    int num = Integer.parseInt(st.getString(0));
+                    switch (st.getString(1)) {
+                        case "d":
+                            Leds[num] = 0;
+                            break;
+                        case "u":
+                            Leds[num] = 1;
+                            break;
+                        case "b":
+                            Leds[num] = 2;
+                    }
+                } else if (jo.has("queue")) {
+                    queues.common =  Integer.parseInt(jo.getString("queue"));
+                } else if (jo.has("qmot")) {
+                    queues.motors =  Integer.parseInt(jo.getString("qmot"));
+                } else if (jo.has("qst1")) {
+                    queues.stepper = Integer.parseInt(jo.getString("qst1"));
+                } else if (jo.has("qdist")) {
+                    queues.dist = Integer.parseInt(jo.getString("qdist"));
+                } else if (jo.has("qleds")) {
+                    queues.leds = Integer.parseInt(jo.getString("qleds"));
+                } else if (jo.has("batt")) {
+                    String s = jo.getString("batt");
+                    if (s.equals("b")) {
+                        Toast.makeText(this, "Внимание! Батарея разряжена!",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, "Внимание! Слишком большая нагрузка!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                } else if (jo.has("pwroff")) {
+                    String s = jo.getString("pwroff");
+                    if (s.equals("k")) {
+                        Toast.makeText(this, "Устройство выключено по нажатию кнопки.",
+                                Toast.LENGTH_LONG).show();
+                    } else if (s.equals("с")) {
+                        Toast.makeText(this, "Устройство выключено по команде пульта.",
+                                Toast.LENGTH_LONG).show();
+                    } else if (s.equals("b")) {
+                        Toast.makeText(this, "Устройство выключено по разряду батареи.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                    mBluetoothLEService.disconnect();
+                    unregisterReceiver(mGattUpdateReceiver);
+                    mConnected = false;
+                    updateConnectionState();
+                    Log.d(TAG, "Power off - " + s);
+                } else if (jo.has("dbg")) {
+                    String s = jo.getString("dbg");
+                    TextView tv = findViewById(R.id.textInfo);
+                    tv.append(s);
                 }
             } catch (JSONException e) {
                 Log.d(TAG, "Cannot unpack JSON data");
             }
-            tvd.append(data);
+            //tvd.append(data);
         }
     }
 
